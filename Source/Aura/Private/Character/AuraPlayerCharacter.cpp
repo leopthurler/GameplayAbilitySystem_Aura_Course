36d9c4a7f30e84bb2aa17e4bf04e78a6280efd0a
Leo Thurler <leo@doubledashstudios.com>
2024-02-09 19:54:16 -0300

    Create Aura Game Mode; Configure Camera in AuraPlayerCharacter; Create BP for AuraPlayerController;

--- Source/Aura/Private/Character/AuraPlayerCharacter.cpp
+++ Source/Aura/Private/Character/AuraPlayerCharacter.cpp
@@ -3,3 +3,16 @@
 
 #include "Character/AuraPlayerCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraPlayerCharacter::AAuraPlayerCharacter()
+{
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0,400,0);
+	GetCharacterMovement()->bConstrainToPlane = true;
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
